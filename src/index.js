9adb280dc51d8db2f0f9a2614d94f41e467c7651
AndanjeW <94687120+AndanjeW@users.noreply.github.com>
2022-05-29 06:52:11 +0300

    added functionality to the weather app

--- src/index.js
+++ src/index.js
@@ -1,66 +1,103 @@
-function dateFormat(date) {
+function formatDate(timestamp){
+  let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+  if(hours < 10){
+      hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+  if(minutes < 10){
+      minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  let day = days[date.getDay()];
+  return `Last updated: ${day} ${hours}:${minutes}`;
+}
+
+function getForecast(coordinates){
+  let apiKey = "e569e71e164e7e0ec0fb7827f996e194";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response){
+let temperatureElement = document.querySelector("#current-temp");
+let cityElement = document.querySelector("#city");
+let windElement = document.querySelector("#wind");
+let humidityElement = document.querySelector("#humidity");
+let dateElement = document.querySelector("#date");
+let conditionsElement = document.querySelector("#weather-condition");
+let iconElement = document.querySelector("#icon");
+
+celsiusTemp = response.data.main.temp;
+
+
+temperatureElement.innerHTML = Math.round(celsiusTemp);
+cityElement.innerHTML= response.data.name;
+windElement.innerHTML= Math.round(response.data.wind.speed);
+humidityElement.innerHTML= response.data.main.humidity;
+conditionsElement.innerHTML = response.data.weather[0].description;
+dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt",response.data.weather[0].description);
+
+getForecast(response.data.coord);
+}
+function search(city) {
+  let apiKey = "e569e71e164e7e0ec0fb7827f996e194";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+
+function handleSearch(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input")
+  search(cityInputElement.value);
+
+}
+
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSearch);
+
+
+
+
+  function formatDay(timestamp){
+      let date = new Date(timestamp * 1000);
+      let day = date.getDay();
+      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      return days[day];
   }
 
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-dateElement.innerHTML = dateFormat(currentTime);
+  function displayForecast(response){
+      let forecast = response.data.daily;
+     let forecastElement = document.querySelector("#forecast");
 
-function showWeather(response) {
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-  document.querySelector("#description").innerHTML = response.data.weather[0].main;
-}
+     let forecastHTML= `<div class="row">`;
+     forecast.forEach(function(forecastDay, index){
+         if (index <5){ 
+      forecastHTML = 
+      forecastHTML + `
+         <div class="col-6">
+            <div class="weather-forecast-day">${formatDay(forecastDay.dt)}</div>
+         </div>
 
-function search(city){
-  let apiKey = "e569e71e164e7e0ec0fb7827f996e194";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeather);
+          <div class="col-6">
+             <div class="weather-forecast-temp">
+                <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png">
+                <span class="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}°</span>
+               <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
+            </div>
+         </div>
+    `;
+  }
+     });
 
-}
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML= forecastHTML;
+  }
 
-function showCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  search(city);
-}
-let searchForm = document.querySelector("#city-search");
-searchForm.addEventListener("submit", showCity);
-function showDegrees(event) {
-  event.preventDefault();
-
-  let tempCelsius = document.querySelector("#temperature");
-  tempCelsius.innerHTML = 30;
-}
-
-let tempCelsius = document.querySelector("#celsius-link");
-tempCelsius.addEventListener("click", showDegrees);
-
-function showDegreesFahrenheit(event) {
-  event.preventDefault();
-  let tempFahrenheit = document.querySelector("#fahrenheit-link");
-  tempFahrenheit.innerHTML = 66;
-}
-
-let tempFahrenheit = document.querySelector("#fahrenheit-link");
-tempFahrenheit.addEventListener("click", showDegreesFahrenheit);
+let celsiusLink = document.querySelector("#celsius-link");
 
 search("Nairobi");
